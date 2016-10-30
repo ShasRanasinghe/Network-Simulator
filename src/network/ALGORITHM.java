4eb31a1a6c85541589dd815528f3d8458cb6b7a0
ShasRanasinghe <shasthraranasinghe@hotmail.com>
2016-10-30 13:09:23 -0400

    Added string representations for the Enums
    added getter for the string
    added constructor to take in a string

--- src/network/ALGORITHM.java
+++ src/network/ALGORITHM.java
@@ -2,5 +2,17 @@ package network;
 
 //Enumerated types of algorithms
 public enum ALGORITHM {
-	RANDOM, FLOODING, SHORTESTPATH, CUSTOM
+	RANDOM("Random"), FLOODING("Flooding"), SHORTESTPATH("Shortest Path"), CUSTOM("Custom");
+
+    private String algString;
+
+    //Create modes from string
+    ALGORITHM(final String alg) {
+        algString = alg;
+    }
+
+    //return as string
+    public String getALGString() {
+        return algString;
+    }
 }
