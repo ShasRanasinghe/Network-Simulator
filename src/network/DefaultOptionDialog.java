3b2ba510cc4a7c0c5277415d718ccee87242e71f
Mohamed Dahrouj <dahrouj.m@gmail.com>
2016-11-20 19:54:00 -0500

    Replaced spaces with tabs in DefaultOptionDialog class

--- src/network/DefaultOptionDialog.java
+++ src/network/DefaultOptionDialog.java
@@ -16,40 +16,39 @@ import javax.swing.JPanel;
  */
 @SuppressWarnings("serial")
 class DefaultOptionDialog extends JDialog {
-	   private JLabel label = new JLabel("Would you like to use the Default or New Network?");
-	   private JButton defaultButton = new JButton("Default");
-	   private JButton newNetworkButton = new JButton("New Network");
-	   
+	private JLabel label = new JLabel("Would you like to use the Default or New Network?");
+	private JButton defaultButton = new JButton("Default");
+	private JButton newNetworkButton = new JButton("New Network");
+   
 	/**
 	 * Constructor 
 	 * @param frame Frame the dialog is added to
 	 * @param title Title of Dialog box
 	 */
 	public DefaultOptionDialog(JFrame frame, String title) {
-		      super(frame, title, false);
-		      this.setLayout(new BorderLayout(10,10));
-		      JPanel panel = new JPanel();
-		      panel.setLayout(new FlowLayout(FlowLayout.CENTER));
-		      panel.add(defaultButton);
-		      panel.add(newNetworkButton);
-
-		      add(label,BorderLayout.CENTER);
-		      add(panel,BorderLayout.SOUTH);
-		      pack();
-		      setLocationRelativeTo(frame);
-		   }
-
-		public void addButtonActionListener(ActionListener listener) {
-			   defaultButton.addActionListener(listener);
-			   newNetworkButton.addActionListener(listener);
-		   }
-		   
-		   public void putDefaultClientProperty(Object key, Object value){
-			   defaultButton.putClientProperty(key, value);
-		   }
-		   
-		   public void putNewNetworkClientProperty(Object key, Object value){
-			   newNetworkButton.putClientProperty(key, value);
-		   }
+		super(frame, title, false);
+		this.setLayout(new BorderLayout(10,10));
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER));
+		panel.add(defaultButton);
+		panel.add(newNetworkButton);
+		add(label,BorderLayout.CENTER);
+		add(panel,BorderLayout.SOUTH);
+		pack();
+		setLocationRelativeTo(frame);
+	}
+	
+	public void addButtonActionListener(ActionListener listener) {
+		defaultButton.addActionListener(listener);
+		newNetworkButton.addActionListener(listener);
+	}
+	
+	public void putDefaultClientProperty(Object key, Object value){
+		defaultButton.putClientProperty(key, value);
+	}
+	
+	public void putNewNetworkClientProperty(Object key, Object value){
+		newNetworkButton.putClientProperty(key, value);
+	}
 	   
 }
